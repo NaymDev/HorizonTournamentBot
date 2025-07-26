a07a9892c1cecad7357313f0e3073fe8881fd92c
NaymDev <125344509+NaymDev@users.noreply.github.com>
2025-07-26 11:05:09 +0200

    feat: ensure parent directory for db file

--- bot/db/session.py
+++ bot/db/session.py
@@ -1,7 +1,13 @@
+import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from db.models import Base
 from config import CONFIG
+
+
+db_path = CONFIG.database.uri.replace('sqlite+aiosqlite:///', '')
+folder = os.path.dirname(db_path)
+os.makedirs(folder, exist_ok=True)
 
 engine = create_async_engine(CONFIG.database.uri, echo=True)
 
